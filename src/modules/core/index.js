bf381b7ee230032c5ccc0d19c3890a3ba6261a6a
Jayanti Gautam <jayanti102024@gmail.com>
2026-07-18 17:10:25 +0530

    feat(core): add core module barrel export

---
+++ src/modules/core/index.js
@@ -0,0 +1,6 @@
+export { EventBus }     from './EventBus.js';
+export { GameLoop }     from './GameLoop.js';
+export { Renderer }     from './Renderer.js';
+export { Camera }       from './Camera.js';
+export { AssetLoader }  from './AssetLoader.js';
+export { DebugOverlay } from './DebugOverlay.js';
